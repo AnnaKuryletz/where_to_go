3f1b95300ebe19eca304f9deb76894fc9a8cd386
anyukurilets <anyu.kutilez@mail.ru>
2025-04-08 21:44:57 +0300

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -114,7 +114,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Убедитесь, что это путь к вашей папке со статикой
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
