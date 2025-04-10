1f4b6f6e5713ba848b816600e4c91022790cf221
anyukurilets <anyu.kutilez@mail.ru>
2025-04-10 18:37:11 +0300

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,15 +22,10 @@ from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.show_phones),
+    path("", views.index),
     path("places/", include("places.urls")),
 ]
 
-from django.views.generic import RedirectView
-
-urlpatterns += [
-    path("", RedirectView.as_view(url="/places/", permanent=True)),
-]
 
 from django.conf import settings
 from django.conf.urls.static import static
