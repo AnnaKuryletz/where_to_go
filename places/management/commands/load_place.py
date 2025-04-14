aabaa118ee2e71f1cf56af0299749ea4fd1173c3
anyukurilets <anyu.kutilez@mail.ru>
2025-04-14 11:23:41 +0300

    Update load_place

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,48 +1,61 @@
 import os
+import json
 import requests
-from urllib.parse import urlparse, unquote
-from pathlib import Path
 
-from django.core.management.base import BaseCommand
-from django.core.files.base import ContentFile
+from pathlib import Path
+from urllib.parse import unquote
+from urllib.parse import urlparse
 
 from places.models import Place, Image
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
+
+
+def get_filename_from_url(url):
+    url_part = urlparse(url).path
+    unquoted_url_part = unquote(url_part)
+    filename = os.path.split(unquoted_url_part)[-1]
+    return filename
+
+
+def load_places(folder):
+    json_files_paths = [
+        os.path.join(folder, filename) for filename in os.listdir(folder)
+        if filename.endswith(".json")
+    ]
+    places_jsons = []
+    for path in json_files_paths:
+        with open(Path(path), 'r') as json_file:
+            places_jsons.append(json.load(json_file))
+    for place_json in places_jsons:
+        place, _ = Place.objects.get_or_create(
+            title=place_json['title'],
+            defaults={
+                "short_description": place_json['description_short'],
+                "long_description": place_json['description_long'],
+                "lng": place_json['coordinates']['lng'],
+                "lat": place_json['coordinates']['lat'],
+            },
+        )
+        for img_url in place_json['imgs']:
+            response = requests.get(img_url)
+            response.raise_for_status()
+            content = ContentFile(response.content)
+            img_name = get_filename_from_url(img_url)
+            image_object = Image.objects.create(place=place)
+            image_object.image.save(name=img_name, content=content, save=True)
 
 
 class Command(BaseCommand):
-    help = 'Loads a place from a JSON URL'
+
+    help = 'Add path to json files folder & images folder'
 
     def add_arguments(self, parser):
-        parser.add_argument('json_url', type=str, help='URL to the JSON file')
-
-    def handle(self, *args, **options):
-        json_url = options['json_url']
-        self.stdout.write(f'Fetching JSON from {json_url}...')
-
-        response = requests.get(json_url)
-        response.raise_for_status()
-        place_json = response.json()
-
-        place, created = Place.objects.get_or_create(
-            title=place_json['title'],
-            defaults={
-                'description_short': place_json.get('description_short', ''),
-                'description_long': place_json.get('description_long', ''),
-                'coordinates_lng': place_json['coordinates']['lng'],
-                'coordinates_lat': place_json['coordinates']['lat'],
-            }
+        parser.add_argument(
+            '-j',
+            '--json_folder',
+            required=True,
         )
 
-        if not created:
-            self.stdout.write(self.style.WARNING(f"Place '{place.title}' already exists. Skipping."))
-            return
-
-        for img_url in place_json.get('imgs', []):
-            img_response = requests.get(img_url)
-            img_response.raise_for_status()
-
-            filename = os.path.basename(unquote(urlparse(img_url).path))
-            image = Image(place=place)
-            image.image.save(filename, ContentFile(img_response.content), save=True)
-
-        self.stdout.write(self.style.SUCCESS(f"Place '{place.title}' successfully loaded."))
+    def handle(self, *args, **options):
+        load_places(options['json_folder'])
