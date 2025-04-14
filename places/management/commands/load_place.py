18e9c0120204864250660d9fdef30083203aaa7c
anyukurilets <anyu.kutilez@mail.ru>
2025-04-14 12:35:15 +0300

    Update load_place7

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -22,8 +22,8 @@ def load_place_from_data(place_json):
     place, created = Place.objects.get_or_create(
         title=place_json['title'],
         defaults={
-            "short_description": place_json['description_short'],
-            "long_description": place_json['description_long'],
+            "short_description": place_json['short_description'],
+            "description_long": place_json['description_long'],
             "lng": place_json['coordinates']['lng'],
             "lat": place_json['coordinates']['lat'],
             "placeId": str(uuid.uuid4()),
