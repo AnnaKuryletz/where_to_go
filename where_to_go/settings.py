c9d183260e1b6fd603aafaf51f26787920ae84a4
anyukurilets <anyu.kutilez@mail.ru>
2025-04-14 03:40:17 +0300

    Cange settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY", default="insecure-default")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost"])
+ALLOWED_HOSTS = "annakurilec.pythonanywhere.com"
 
 
 # Application definition
