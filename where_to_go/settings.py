f6094f3039f096a5963a8376551a181d18ac0ad9
anyukurilets <anyu.kutilez@mail.ru>
2025-04-16 17:14:10 +0300

    Added default value for ALLOWED_HOSTS

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -19,9 +19,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
-
-
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 # Application definition
 
