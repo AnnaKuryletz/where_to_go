3351bd94a688fa911bb6deefcc9a0b173a97f8f1
anyukurilets <anyu.kutilez@mail.ru>
2025-04-16 17:29:43 +0300

    The title field has become unique

--- places/models.py
+++ places/models.py
@@ -3,7 +3,7 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Название локации")
+    title = models.CharField(max_length=200, verbose_name="Название локации", unique=True)
     short_description = models.TextField(blank=True, verbose_name="Краткое описание")
     long_description = HTMLField(blank=True, verbose_name="Подробное описание")
     lng = models.FloatField(verbose_name="Долгота")
